6fe60a125839838c0d37691eac07dc54766cc771
Aakash <82134486+aakash123254@users.noreply.github.com>
2024-08-04 01:21:49 +0530

    Add files via upload

---
+++ EditDistance.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int editDistance(String str1, String str2) {
+        int n=str1.length();
+        int m=str2.length();
+        int dp[][] =new int[n+1][m+1];
+
+        for(int[]arr:dp){
+            Arrays.fill(arr,-1);
+        }
+        return util(str1,str2,n,m,dp);
+    }
+    int util(String str1, String str2, int n, int m, int[][]dp){
+        if(n==0){
+            return m;
+        }
+        if(m==0){
+            return n;
+        }
+        if(dp[n][m]!=-1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1)==str2.charAt(m-1)){
+            return dp[n][m] = util(str1,str2,n-1,m-1,dp);
+        }
+        return dp[n][m] = Math.min(util(str1,str2,n,m-1,dp)+1,
+                Math.min(util(str1,str2,n-1,m,dp)+1, util(str1,str2,n-1,m-1,dp)+1));
+    }
+}
